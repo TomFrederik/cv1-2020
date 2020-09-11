84d08b2aac2b395ec8bd465b5a8568bdc4a77fc8
TomFrederik <tlieberum@outlook.de>
2020-09-11 12:41:54 +0200

    updated idd_image_formation and plot

--- lab1/intrinsic_images/idd_image_formation.py
+++ lab1/intrinsic_images/idd_image_formation.py
@@ -1,21 +1,27 @@
-def main():
-    import numpy as np
-    from PIL import Image
-    from matplotlib import pyplot as plt
-    #int32 to prevent overflow of the standard uint8 during element-wise multiplication
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+
+
+def main():    
+    # int32 to prevent overflow of the standard uint8 during element-wise multiplication
     original = np.array(Image.open('ball.png'))
-    print(np.max(original))
     shading = np.array(Image.open('ball_shading.png'), dtype="int32")
     albedo = np.array(Image.open('ball_albedo.png'), dtype="int32")
-    #for broadcasting shapes
-    shading = np.expand_dims(shading,-1)
 
-    #I(x)=R(X) x S(x) for all x in one line
-    #/255 to renormalise
-    reconstruction = np.array(np.multiply(shading,albedo)/256,dtype="uint8")
-    print(np.linalg.norm(original-reconstruction))
+    # I(x)=R(X) x S(x) for all x in one line
+    # divide by 255 to renormalise
+    # dividing by 256 gives 0 RMS
+    print(np.max(albedo*shading[...,None])/255)
+    print(np.max(albedo*shading[...,None])/256)
+    print(np.max(albedo))
+    print(np.max(shading))
+    reconstruction_255 = np.array(albedo*shading[:,:,None]/255,dtype="int32")
+    reconstruction_256 = np.array(albedo*shading[:,:,None]/256,dtype="int32")
+    print('RMS_255 = {0:1.4f}'.format(np.sqrt(np.mean((reconstruction_255-original)**2))))
+    print('RMS_256 = {0:1.4f}'.format(np.sqrt(np.mean((reconstruction_256-original)**2))))
 
-    fig=plt.figure()
+    fig = plt.figure()
     plt.subplot(2, 2, 1)
     plt.imshow(original)
     plt.title('Original')
@@ -24,12 +30,13 @@ def main():
     plt.title('Shading')
     plt.subplot(2, 2, 3)
     plt.imshow(albedo)
+    plt.ylabel('y')
+    plt.xlabel('x')
     plt.title('Albedo')
     plt.subplot(2, 2, 4)
-    plt.imshow(reconstruction)
+    plt.imshow(reconstruction_255)
     plt.title('Reconstruction')
-
-    plt.show()
+    plt.savefig('image_reconstruction.pdf')
 
 if __name__ == "__main__":
     main()
