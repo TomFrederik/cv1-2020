4fa3e2e47cdd7e83153f8db83bf96f25da84497f
TomFrederik <tlieberum@outlook.de>
2020-09-18 10:02:03 +0200

    fixed myPSNR and worked on denoise

--- lab2/Image_enhancement/denoise.py
+++ lab2/Image_enhancement/denoise.py
@@ -9,7 +9,7 @@ def denoise( image, kernel_type, **kwargs):
     elif kernel_type == 'median':
         imOut = cv2.medianBlur(image, kwargs['kernel_size'])
     elif kernel_type == 'gaussian':
-        imOut = cv2.GaussianBlur(image, kwargs['kernel_size'], sigma) # use our function here? Instructions unclear.
+        imOut = cv2.GaussianBlur(image, (kwargs['kernel_size'], kwargs['kernel_size']), kwargs['sigma'])
     else:
         print('Operation Not implemented')
         return None
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     plt.show()
     psnrs = np.zeros((4,3))
 
+    '''
     for i,kernel_size in enumerate([3,5,7]):
         kwargs = {'kernel_size':kernel_size}
 
@@ -55,7 +56,7 @@ if __name__ == "__main__":
         psnrs[3,i] = myPSNR(img, out_im)
     
     print('PSNRs = ',psnrs)
-    
+
     # write psnrs to file
     file = open(out_dir + 'box_median_psnr.txt', 'w')
     file.write('kernel_size         =   3   |   5   |   7  \n')
@@ -65,3 +66,41 @@ if __name__ == "__main__":
     file.write('PSNR Median (SP)    = {0:1.2f} | {1:1.2f} | {2:1.2f}\n'.format(*tuple([psnrs[2,i] for i in range(3)])))
     file.write('PSNR Median (Gauss) = {0:1.2f} | {1:1.2f} | {2:1.2f}'.format(*tuple([psnrs[3,i] for i in range(3)])))
     file.close() 
+    '''
+
+    # denoising gauss noise with gaussian
+    
+    kernels = np.arange(3,13,2)
+    sigma = np.arange(0.1,4.1,0.1)
+    gauss_psnr = np.zeros((len(kernels),len(sigma)))
+
+    for i in range(len(kernels)):
+        for j in range(len(sigma)):
+
+            kwargs = {'kernel_size':kernels[i], 'sigma':sigma[j]}
+
+            print('Denoising gaussian image with [{0}x{0}] Gaussian filter and sigma {1:1.1f}..'.format(kernels[i], sigma[j]))
+            out_im = denoise(gauss_img, 'gaussian', **kwargs)
+            print('./gaussian_results/gaussian_gaussian_ks_{0}_sig_{1:1.1f}.jpg'.format(kernels[i],sigma[j]))
+            #cv2.imwrite('./gaussian_results/gaussian_gaussian_ks_{0}_sig_{1:1.1f}.jpg'.format(kernels[i],sigma[j]), out_im)
+            plt.figure()
+            plt.imshow(out_im)
+            plt.savefig(out_dir + './gaussian_results/gaussian_gaussian_ks_{0}_sig_{1:1.1f}.pdf'.format(kernels[i],sigma[j]))
+            plt.close()
+            gauss_psnr[i,j] = myPSNR(img, out_im)
+        
+    # write psnrs:
+    file = open(out_dir + 'gaussian_results/gauss_filter.txt','w')
+    file.write(len(kernels)*'c | '+'c \n')
+    file.write('$\sigma$ \\backslash kernel size')
+    for i in range(len(kernels)):
+        file.write('& {}'.format(int(kernels[i])))
+    file.write('\\\\ \n\hline \hline \n')
+
+    for i in range(len(sigma)):
+        file.write('{0:1.1f}'.format(sigma[i]))
+        for j in range(len(kernels)):
+            file.write('& {0:1.2f}'.format(gauss_psnr[j,i]))
+        file.write('\\\\ \n\hline \n')
+    
+    file.close()
--- lab2/Image_enhancement/myPSNR.py
+++ lab2/Image_enhancement/myPSNR.py
@@ -5,13 +5,18 @@ import PIL.Image as im # for loading the image
 
 def myPSNR( orig_image, approx_image ):
 
+    
+    # convert from uint8 to float, otherwise calculation will be off due to overflow/precision errors
+    orig_image = np.asarray(orig_image, dtype=np.float64)
+    approx_image = np.asarray(approx_image, dtype=np.float64)
+
     # compute maximum and RMSE
     I_max = np.max(orig_image)
     RMSE = np.sqrt(np.mean((orig_image - approx_image) ** 2))
     
     # compute PSNR
     PSNR = 20 * np.log10(I_max / RMSE)
-    
+
     return PSNR
 
 if __name__ == "__main__":
@@ -27,9 +32,9 @@ if __name__ == "__main__":
 
     print('Computing myPSNR between image1_gaussian and image1...')
     # load images
-    image1 = np.array(im.open('./images/image1.jpg'))
     image1_gaussian = np.array(im.open('./images/image1_gaussian.jpg'))
     
     # compute PSNR
     PSNR_gaussian_1 = myPSNR(image1, image1_gaussian)
     print('PSNR = {0:1.3f}'.format(PSNR_gaussian_1))
+
