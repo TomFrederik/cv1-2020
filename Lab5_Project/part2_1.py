c20ae4ddb09895f91641567ac94f0c5b140b205a
TomFrederik <tlieberum@outlook.de>
2020-10-14 17:45:10 +0200

    minor changes in part 2

---
+++ Lab5_Project/part2_1.py
@@ -0,0 +1,10 @@
+#import torch
+#import torchvision
+#import torchvision.transforms as transforms
+#import torch.nn as nn
+#import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+#import torch.optim as optim
+
+print('hi')
